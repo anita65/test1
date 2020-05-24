2a4b485da9e43a1054951d0c84fd493838e6d39a
kish <kishorimore637@gmail.com>
2020-05-24 17:12:13 +0530

    d2 in web

--- MyWebApp.java
+++ MyWebApp.java
@@ -1,7 +1,7 @@
 package com.kishori;
 
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
-
+//d2
 public class MyWebApp extends AbstractAnnotationConfigDispatcherServletInitializer {
 
     @Override
