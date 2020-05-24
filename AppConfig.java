fdaec170e166f348cf1c2f4603f3967f7583a9ae
kish <kishorimore637@gmail.com>
2020-05-24 17:13:30 +0530

    m1 in app

--- AppConfig.java
+++ AppConfig.java
@@ -14,7 +14,7 @@ import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
-
+//m1
 @Configuration
 @ComponentScan(basePackages = "com.kishori")
 @EnableJpaRepositories(basePackages = "com.kishori.repository")
